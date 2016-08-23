aff0710911b16fe0d79e783fc763ecbd47bc1b89
Toby Chow <toby_chow@hotmail.com>
2016-08-22 21:22:21 -0400

    added fade animation

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -11,13 +11,14 @@ $(document).ready(function() {
 
     // List of streamers to display
     var streamers = ['esl_csgo', 'freecodecamp', 'voyboy', 'reynad27', 'joshog', 'Kolento', 'potato', 'brunofin', 'comster404', 'nalcs1', 'nalcs2'];
-
+    var numStreamers = streamers.length;
     // can use result object after resolve to clean up global namespace
     var streamLink;
     var status;
     var channelLogo;
     var previewPic;
     var isOnline;
+    var count = 0;
 
     // Get twitch API info and displays information
     function twitchAPI(query) {
@@ -78,21 +79,21 @@ $(document).ready(function() {
         if (streamer) {
             // Creates display after each streamer's data is obtained
             twitchAPI(streamer).then(function(result) {
-                console.log(result);
-                // IIFE - Creates individual container for each streamer
-                (function createDisplay() {
-                    var logo;
-                    var image = '<img class="preview" src="' + previewPic + '">';
-                    //checks if channel logo exists
-                    if (channelLogo !== null) {
-                        logo = '<img class="logo" src="' + channelLogo + '">';
-                    } else {
-                        logo = "";
-                    }
-                    // Creates html structure for each streamer                  
-                    var bsCol = `<div class="col-md-4 col-sm-6 col-xs-12 streamer ${streamer}">`;
-                    $('.row').append(
-                        `${bsCol}
+                    console.log(result);
+                    // IIFE - Creates individual container for each streamer
+                    (function createDisplay() {
+                        var logo;
+                        var image = '<img class="preview" src="' + previewPic + '">';
+                        //checks if channel logo exists
+                        if (channelLogo !== null) {
+                            logo = '<img class="logo" src="' + channelLogo + '">';
+                        } else {
+                            logo = "";
+                        }
+                        // Creates html structure for each streamer                  
+                        var bsCol = `<div class="col-md-4 col-sm-6 col-xs-12 streamer ${streamer}">`;
+                        $('.row').append(
+                            `${bsCol}
                 <div class="overlay">
                     <a href="https://www.twitch.tv/${streamer}" target="_blank">
                         <i class="fa fa-play-circle fa-5x play-btn" aria-hidden="true" style="display:none"></i>
@@ -104,26 +105,44 @@ $(document).ready(function() {
                 <div class="status">${status}</div>
                 <div class="previewPic">${image}</div>
                 `);
-                })();
+                    })();
 
-                // Shows play button on hover
-                $('.overlay,.preview').hover(function() {
-                        var overlay = $(this).parent('.previewPic').siblings('.overlay ');
-                        overlay.addClass('play');
-                        var playBtn = $(this).find('.play-btn');
-                        console.log(playBtn);
-                        $(playBtn).css('display', 'inline-block');
-                    },
-                    function() {
-                        $(this).removeClass('play'); // context of 'this' is changed??
-                        $('.play-btn').hide();
-                    });
+                    // Shows play button on hover
+                    $('.overlay,.preview').hover(function() {
+                            var overlay = $(this).parent('.previewPic').siblings('.overlay ');
+                            var name = $(this).parent('.previewPic').siblings('.name ');
+                            var status = $(this).parent('.previewPic').siblings('.status');
+                            var playBtn = $(this).find('.play-btn');
+                            overlay.addClass('play');
+                            $(playBtn).css('display', 'inline-block');
+                            // $(name).add(status).fadeOut(); //.add to add multiple variable selectors
+                            $(status).slideUp();
+                        },
+                        function() {
+                            $(this).removeClass('play'); // context of 'this' is changed??
+                            $('.play-btn').hide();
+                            var name = $(this).siblings('.name');
+                            console.log(name);
+                            var status = $(this).siblings('.status');
+                            $(status).slideDown();
+                        });
 
-                //Only create next streamer's display after current streamer is complete to preserve the array's order
-                display(streamers);
-            });
+                    // track streamers info that are ready to be displayed
+                    count++;
+
+                    //Only create next streamer's display after current streamer is complete to preserve the array's order
+                    display(streamers);
+                })
+                // Displays content and removes loading icon once all content is loaded (prevents loading each display one by one)
+                .then(function() {
+                    if (count === numStreamers) {
+                        $('.loading').css('display', 'none');
+                        $('.streamer').css('display', 'inline-block');
+                    }
+                });
         }
     }
+
     // Init
     display(streamers);
 });
